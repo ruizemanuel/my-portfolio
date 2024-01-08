5bc93452edeada5a5f468bf0ea5264662f36db4c
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-08 09:18:40 -0300

    portfolio init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import styled, { ThemeProvider } from 'styled-components'
+import { darkTheme } from './utils/Themes';
+
+const Body = styled.div`
+background-color: ${({theme}) => theme.bg};
+width: 100%;
+height: 100%;
+overflow-x: hidden;
+`;
+
+function App() {
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Body>Portfolio</Body>
+    </ThemeProvider>
+  )
+}
+
+export default App
